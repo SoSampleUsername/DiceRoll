cfa429a0ff0f7c1ab9ec8c74f4dbe284cc1e770b
SoSampleUsername <mamichdok@gmail.com>
2020-02-03 15:57:03 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DiceRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 'Enter' for Player 1 to roll a dice ");
+            Console.ReadLine();
+            Random player1 = new Random();
+            int n = player1.Next(1, 7);
+            Console.WriteLine($"Player 1 rolled {n}\n");
+            Console.WriteLine("Press 'Enter' for Player 2 to roll a dice ");
+            Console.ReadLine();
+            Random player2 = new Random();
+            int m = player2.Next(1, 7);
+            Console.WriteLine($"Player 2 rolled {m}\n");
+            
+            if (m>n)
+            {
+                Console.WriteLine("Player 2 won the game");
+            }
+            else if (m<n)
+            {
+                Console.WriteLine("Player 1 won the game");
+            }
+            else if (m==n)
+            {
+                Console.WriteLine("It's a tie");
+            }
+        }
+    }
+}
